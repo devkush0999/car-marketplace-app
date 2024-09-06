ff10b39018c37ec1248423cfb94fbcb69d867b16
devesh0999 <devesh2cse@gmail.com>
2024-09-06 18:32:10 +0530

    added the local data section

---
+++ src/components/Shared/Data.jsx
@@ -0,0 +1,107 @@
+const Cars =[
+   {
+    id: 1,
+    condition: "New"
+} ,
+{
+    id: 2,
+    condition: "Good"
+} ,
+{
+    id: 3,
+    condition: "Old"
+} 
+
+
+]
+const CarMakes = [
+    {
+        id: 1,
+        name: 'Toyota',
+        model: 'Corolla',
+    },
+    {
+        id: 2,
+        name: 'Honda',
+        model: 'Civic',
+    },
+    {
+        id: 3,
+        name: 'Ford',
+        model: 'Mustang',
+    },
+];
+
+const Pricing = [
+    {
+        id: 1,
+        name: 'Basic',
+        price: 1000,
+    },
+    {
+        id: 2,
+        name: 'Premium',
+        price: 2000,
+    },
+    {
+        id: 3,
+        name: 'Elite',
+        price: 3000,
+    },
+];
+
+const carCategory = [
+    {
+      id: 1,
+      name: "SUV",
+      icon: "https://placehold.co/40x40"
+    },
+    {
+      id: 2,
+      name: "Sedan",
+      icon: "https://placehold.co/40x40"
+    },
+    {
+      id: 3,
+      name: "Hatchback",
+      icon: "https://placehold.co/40x40"
+    },
+    {
+      id: 4,
+      name: "Convertible",
+      icon: "https://placehold.co/40x40"
+    },
+    {
+      id: 5,
+      name: "Coupe",
+      icon: "https://placehold.co/40x40"
+    },
+    {
+      id: 6,
+      name: "Pickup Truck",
+      icon: "https://placehold.co/40x40"
+    },
+    {
+      id: 7,
+      name: "Minivan",
+      icon: "https://placehold.co/40x40"
+    },
+    {
+      id: 8,
+      name: "Electric",
+      icon: "https://placehold.co/40x40"
+    },
+    {
+      id: 9,
+      name: "Luxury",
+      icon: "https://placehold.co/40x40"
+    }
+  ];
+  
+
+export default {
+    Cars,
+    CarMakes,
+    Pricing,
+    carCategory
+}
