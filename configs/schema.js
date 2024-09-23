3a58603a4ffd673c3009feac33c542dfc67fe914
devesh0999 <devesh2cse@gmail.com>
2024-09-23 23:27:55 +0530

    added

--- configs/schema.js
+++ configs/schema.js
@@ -1,5 +1,28 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { pgTable, varchar, serial } from "drizzle-orm/pg-core";
 
 export const carListing=pgTable('carListing', {
-    id:
+    id:serial('id').primaryKey(),
+    listingTitle:varchar('listingTitle').notNull(),
+    tagline:varchar('tagline'),
+    originalPrice:varchar('originalPrice'),
+    sellingPrice:varchar('sellingPrice'),
+    category:varchar('category').notNull(),
+    condition:varchar('condition').notNull(),
+    type:varchar('type').notNull(),
+    model:varchar('model'),
+    mileage:varchar('mileage'),
+    year:varchar('year'),
+    drivetype:varchar('drivetype'),
+    transmission:varchar('transmission'),
+    make:varchar('make'),
+    fueltype:varchar('fueltype'),
+    engineSize:varchar('engineSize'),
+    price:varchar('price'),
+    listingdescription:varchar('listingdescription'),
+    offertype:varchar('offertype'),
+    cylinder:varchar('cylinder'),
+    vin:varchar('vin'),
+    offertype:varchar('offertype'),
+    door:varchar('door'),
+    color:varchar('color')
 })
---
+++ src/add-listing/components/CheckBoxField.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CheckBoxFeild() {
+  return (
+    <div>CheckBoxFeild</div>
+  )
+}
+
+export default CheckBoxFeild
